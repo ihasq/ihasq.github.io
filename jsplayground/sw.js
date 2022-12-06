64ee45f2732a76e46d201e439b3379ab9d98b7f6
ihasq <91711202+ihasq@users.noreply.github.com>
2022-12-06 11:45:43 +0900

    Add files via upload

--- jsplayground/sw.js
+++ jsplayground/sw.js
@@ -1,60 +1,15 @@
-const CACHE_VERSION = 'v1';
-const CACHE_NAME = `${registration.scope}!${CACHE_VERSION}`;
+//'use strict';
 
-const urlsToCache = [
-  '.',
-  'index.html',
-  'icons/48.jpg',
-  'icons/72.jpg',
-  'icons/96.jpg',
-  'icons/144.jpg',
-  'icons/168.jpg',
-  'icons/192.jpg',
-];
+/* global registration */
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', function (event) {
+  // skip
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return cacheNames.filter((cacheName) => {
-        return cacheName.startsWith(`${registration.scope}!`) && cacheName !== CACHE_NAME;
-      });
-    }).then((cachesToDelete) => {
-      return Promise.all(cachesToDelete.map((cacheName) => {
-        return caches.delete(cacheName);
-      }));
-    })
-  );
+self.addEventListener('activate', function (event) {
+  // skip
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-    .then((response) => {
-      if (response) {
-        return response;
-      }
-      let fetchRequest = event.request.clone();
-      return fetch(fetchRequest)
-        .then((response) => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-          let responseToCache = response.clone();
-          caches.open(CACHE_NAME)
-            .then((cache) => {
-              cache.put(event.request, responseToCache);
-            });
-          return response;
-        });
-    })
-  );
+self.addEventListener('fetch', function (event) {
+  // skip
 });
